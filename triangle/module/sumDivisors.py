70a3aae76c8fc7775f2713a3363d7453f0f4e564
johan-ventus <66406539+johan-ventus@users.noreply.github.com>
2020-06-12 15:46:02 +0300

    Jv#file write (#2)
    * basic functionality
    
    * refactoring
    
    Co-authored-by: Johan Ventus <johan.ventus@nightingalehealth.com>

--- triangle/module/sumDivisors.py
+++ triangle/module/sumDivisors.py
@@ -1,24 +1,36 @@
+from io import StringIO
+import sys
+
 def calculateSumAndDivisors(ordinal):
     triangle_numbers = []
     terms = []
     factor = 0
-    
 
     def terms_output():
         return str((print(*terms, sep=',')))
 
+    def printToFile():
 
-    for i in range(ordinal()):
+        old_stdout = sys.stdout
+        result = StringIO()
+        sys.stdout = result
+
+        print(factor, end = ':')
+        terms_output()
+
+        sys.stdout = old_stdout
+        result_string = result.getvalue()
+        print(result_string)
+
+        File_object = open("Divisors and sum of "+str(ordinal)+"th term.out", "w")
+        File_object.write(result_string)
+
+    for i in range(ordinal):
         triangle_numbers.append(i+1)
         factor = sum(triangle_numbers)
-
 
     for i in range(1, factor+1):
         if sum(triangle_numbers) % i == 0:
             terms.append(i)
-
-    print(factor, end = ':')
-    terms_output()
-
-def askOrdinal():
-    return int(input('\nGimme the ordinal\n'))
+    
+    printToFile()
